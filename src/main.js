c856d20dd328337408a5d5b7f86199d730f55103
MiguelBrito1086 <mbrito1086@gmail.com>
2018-02-27 14:51:30 -0500

    input value rendering on dom

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Message from './Message.vue';
+// import Input from './Input.vue';
+
+Vue.component('app-message', Message);
+// Vue.component('app-input', Input);
 
 new Vue({
   el: '#app',
